4cd8a11a682c0294112aa78576211a357b1bef3d
Victor Mikhaylov <victorm137@gmail.com>
2025-01-23 17:43:13 +0400

    Buy stocks

---
+++ src/main/java/slidingwindow/buystock/Solution.java
@@ -0,0 +1,25 @@
+package slidingwindow.buystock;
+
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+ *
+ */
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > profit) {
+                profit = prices[i] - buy;
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+}
